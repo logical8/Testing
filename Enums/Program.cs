178d9e78ed3fdddf418ece2e23fc1424d5d24be8
alekkireev <alekkireev@gmail.com>
2023-06-11 17:12:57 +0400

    Testing Enums library

---
+++ Enums/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using KeepCorrect.Common.Extensions;
+
+public enum Enum1
+{
+    Unknown,
+    First,
+    Second
+}
+
+public enum Enum2
+{
+    Unk,
+    Fir,
+    Sec
+}
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var result = Enum1.First.ToEnumByInt<Enum1, Enum2>();
+        Console.WriteLine(result);
+    }
+}
+
